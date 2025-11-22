647cf708b7f15e3d6a5e16eba0f2bf14c2eba895
gnanaparthiban <gnanaparthiban@xtowns.in>
2025-11-22 19:28:08 +0530

    dashboard changes

--- src/dashboard/pages/StatCard.jsx
+++ src/dashboard/pages/StatCard.jsx
@@ -4,16 +4,16 @@ import { RightOutlined } from "@ant-design/icons";
 const StatCard = ({ title, value, meta, gradient, icon }) => {
   return (
     <div
-      className="rounded-xl overflow-hidden h-[150px] shadow-[0_10px_30px_rgba(2,6,23,0.06)]"
+      className="rounded-xl overflow-hidden h-[140px] shadow-[0_10px_30px_rgba(2,6,23,0.06)]"
       style={{ background: gradient }}
     >
-      <div className="p-4 flex justify-between items-center text-white">
+      <div className="p-5 flex justify-between items-center text-white">
         
         {/* LEFT SECTION */}
         <div className="flex flex-col gap-1">
-          <p className="">{title}</p>
-          <p className="text-3xl  leading-none">{value}</p>
-          <p className="text-sm opacity-90">{meta}</p>
+          <p className="text-[13px]">{title}</p>
+          <p className="text-2xl  leading-none">{value}</p>
+          <p className="text-[10px]  opacity-90">{meta}</p>
         </div>
 
         {/* RIGHT SECTION */}
